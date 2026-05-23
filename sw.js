12c3785ae6770c1a6104433255f9473f63263822
Mateo <mateo.ou@gmail.com>
2026-05-23 18:54:53 +0200

    fix: upgrade sw.js to Network-First to clear cache traps and enforce latest design rendering

--- sw.js
+++ sw.js
@@ -1,15 +1,40 @@
+const CACHE_NAME = 'sgf-v2';
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('sgf-v1').then(c => c.addAll([
-    './','./index.html','./css/style.css',
-    './js/storage.js','./js/gamification.js','./js/ui.js','./js/data.js'
-  ])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll([
+      './',
+      './index.html',
+      './css/style.css',
+      './js/storage.js',
+      './js/gamification.js',
+      './js/ui.js',
+      './js/data.js'
+    ]))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => {
+        if (k !== CACHE_NAME) return caches.delete(k);
+      })
+    )).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  if (e.request.url.startsWith(self.location.origin)) {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  }
 });
