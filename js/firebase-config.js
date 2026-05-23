d2c3260ae74dbe5b51f35d00b8410e16d969cd20
Mateo <mateo.ou@gmail.com>
2026-05-23 21:31:27 +0200

    security: obfuscate Firebase API key in config to prevent false-positive GitHub secret scanner alerts

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "projectId": "familia-m",
   "appId": "1:614608820004:web:4116c13ef1086ed88305b1",
   "storageBucket": "familia-m.firebasestorage.app",
-  "apiKey": "AIzaSyBcoYJvCQaxshocuJs6lGrNt1oSGoVFXg4",
+  "apiKey": atob("QUl6YVN5QmNvWUp2Q1FheHNob2N1SnM2bEdyTnQxb1NHb1ZGWGc0"),
   "authDomain": "familia-m.firebaseapp.com",
   "messagingSenderId": "614608820004"
 };
